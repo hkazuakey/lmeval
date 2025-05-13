8dbeb747c30a70583457c0e5937c4b3ab9940108
Aaron Schneider <90160014+schneidergithub@users.noreply.github.com>
2025-05-12 21:57:55 -0400

    Update adversarial_prompts.py (small typo)
    The word "thoughts" fixed.

--- lmeval/prompts/adversarial_prompts.py
+++ lmeval/prompts/adversarial_prompts.py
@@ -18,7 +18,7 @@ from ..enums import TaskType
 class AdversarialChainOfThoughtsPrompt(Prompt):
     def __init__(self,
                 template: str = "{{question.question}} Let's think step by step and answer the question starting with Answer:",
-                name: str = "Adversarial Chain of Thoutghts",
+                name: str = "Adversarial Chain of Thoughts",
                 description: str = "Ask the model to think steps by steps before answering the question",
                 task_type = TaskType.text_generation,
                 url: str = '',
